ba2a686cbf516ab12977e392ec694a7f22673e09
Faith6913 <zhihao_2023@163.com>
2022-08-21 22:36:19 +0800

    Completed Avatar Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>
-    <h1>App 组件</h1>
-    <button>按钮</button>
+  <div class="root">
+    <Avatar
+      url="https://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg"
+      :size="100"
+    />
+    <Avatar
+      url="https://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg"
+      :size="600"
+    />
   </div>
 </template>
 
+<script>
+import Avatar from "./components/Avatar";
+export default {
+  name: "App",
+  components: {
+    Avatar,
+  },
+};
+</script>
 
-
+<style></style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+    class="avatar-img"
+    :src="url"
+    alt="Avatar"
+    :style="{
+      width: size + 'px',
+      height: size + 'px',
+    }"
+  />
+</template>
+
+<script>
+export default {
+  name: "AvatarComponent",
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style>
+.avatar-img {
+  border-radius: 50%;
+  object-fit: cover;
+  display: block;
+}
+</style>
