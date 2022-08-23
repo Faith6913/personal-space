66e644711512612915f5b29db940f913505f43aa
Faith6913 <zhihao_2023@163.com>
2022-08-23 11:47:10 +0800

    优化工程结构失败

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <template>
   <div>
-    <h1>分页组件</h1>
-    <Pager
-      :current="curPage"
-      :total="410"
-      :limit="30"
-      :visibleNumber="10"
-      @pageChange="handlePageChange($event)"
-    />
+    <h1>App组件</h1>
   </div>
 </template>
 
 <script>
-// import Avatar from "./components/Avatar";
-// import Icon from "./components/Icon";
-import Pager from "./components/Pager";
-export default {
-  name: "App",
-  data() {
-    return {
-      curPage: 1,
-    };
-  },
-  components: {
-    Pager,
-  },
-  methods: {
-    handlePageChange(newPage) {
-      console.log(
-        `这里是父组件，本函数用于处理分页子组件的点击事件,以达到显示第${newPage}页的效果`
-      );
-      this.curPage = newPage;
-    },
-  },
-};
+
 </script>
 
 <style scoped></style>
 
-/* @pageChange="handlePageChange($event)" */
---
+++ src/components/Pager/test.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <Pager
+      :current="curPage"
+      :total="410"
+      :limit="30"
+      :visibleNumber="10"
+      @pageChange="handlePageChange($event)"
+    />
+  </div>
+</template>
+
+<script>
+import Pager from "./index";
+export default {
+  name: "App",
+  data() {
+    return {
+      curPage: 1,
+    };
+  },
+  components: {
+    Pager,
+  },
+  methods: {
+    handlePageChange(newPage) {
+      console.log(
+        `这里是父组件，本函数用于处理分页子组件的点击事件,以达到显示第${newPage}页的效果`
+      );
+      this.curPage = newPage;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
+
+/* @pageChange="handlePageChange($event)" */
