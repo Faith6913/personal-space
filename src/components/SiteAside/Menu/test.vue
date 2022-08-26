628b35cba7727994eb0d1ebacd73a5e6c0163b96
Faith6913 <zhihao_2023@163.com>
2022-08-26 20:05:03 +0800

    Menu Component Completed

---
+++ src/components/SiteAside/Menu/test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test-container">
+    <Menu />
+  </div>
+</template>
+
+<script>
+import Menu from "./";
+export default {
+  components: {
+    Menu,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/styles/var.less";
+.test-container {
+  width: 250px;
+  height: 600px;
+  background-color: #000;
+  border: 1px solid red;
+}
+</style>
